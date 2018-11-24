29e8d3d674688e138f21935b6dffbe93f921cf8e
G-Wang <ywa289@sfu.ca>
2018-11-24 15:34:11 -0800

    add audio preprocessing

---
+++ hparams.py
@@ -0,0 +1,14 @@
+class hparams:
+    # audio parameters
+    num_mels = 80
+    fmin = 125
+    fmax = 7600
+    fft_size = 1024
+    hop_size = 256
+    sample_rate = 22050
+    preemphasis = 0.97
+    min_level_db = -100
+    ref_level_db = 20
+    rescaling = False
+    rescaling_max = 0.999
+    allow_clipping_in_normalization = True
