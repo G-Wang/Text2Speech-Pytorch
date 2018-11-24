2e508f6ca4c77ea467e1487cf15ad2fc3a2228c1
G-Wang <ywa289@sfu.ca>
2018-11-24 15:20:03 -0800

    add text preprocessing with english

---
+++ tts/frontend/__init__.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+
+"""Text processing frontend
+
+All frontend module should have the following functions:
+
+- text_to_sequence(text, p)
+- sequence_to_text(sequence)
+
+and the property:
+
+- n_vocab
+
+"""
+from tts.frontend import en
